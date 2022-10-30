d47bdef562a9e8ad5a2ed15be7a0c13c75da5e7f
sanjeev varma <sanjeev@kali.sanjeev>
2022-10-30 15:15:46 +0530

    first commit

---
+++ a.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+	int i,n;
+	printf("Enter the value of n = ");
+	scanf("%d",&n);
+
+
+	//write a for loop 
+	for(i=0;i<=n;i++) {
+		printf("%d",i);
+	}
+	return 0;
+}
